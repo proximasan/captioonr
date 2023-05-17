928a237eefd08ed045d40d1d87b763cbfb7a02e3
proximasan <proximanova@posteo.net>
2023-05-17 16:46:37 +0200

    add slider

--- captioonr.py
+++ captioonr.py
@@ -15,13 +15,13 @@ class ImageCaptionGUI(ThemedTk):
         self.title("☆━━━━CAPTIOONR━━━━☆")
 
         # Set the window size
-        self.geometry("840x880")
+        self.geometry("840x920")
 
         # Center the window on the screen
         self.center_window()
 
         self.set_theme("equilux")
-        self.configure(bg="#464646")  # set background color to dark gray
+        self.configure(bg="#464646")
 
         self.folder_path = StringVar()
         self.caption_text = StringVar()
@@ -29,12 +29,18 @@ class ImageCaptionGUI(ThemedTk):
         self.image_index = 0
 
         self.content_frame = ttk.Frame(self)
-        self.content_frame.place(anchor='center', relx=0.5, rely=0.5)
+        self.content_frame.pack(expand=True)
+
+        self.image_on_canvas = None
+        self.text_was_edited = False
 
         self.image_file_label = ttk.Label(self.content_frame, text="")
-        self.image_file_label.grid(row=2, column=1)
+        self.image_file_label.grid(row=2, column=1, sticky=E, pady=(0, 5))
         if self.image_files:
             self.image_file_label.config(text=self.truncate_filename(self.image_files[self.image_index]))
+
+        self.image_count_label = ttk.Label(self.content_frame, text="")
+        self.image_count_label.grid(row=2, column=2, pady=(0, 5))
 
         self.auto_save_mode = BooleanVar()
 
@@ -47,16 +53,16 @@ class ImageCaptionGUI(ThemedTk):
         self.bind('<Control-s>', lambda e: self.save_and_show_done())
 
         self.folder_label = ttk.Label(self.content_frame, text="Folder:")
-        self.folder_label.grid(row=0, column=0, sticky=E)
+        self.folder_label.grid(row=0, column=0, sticky=E, pady=(10, 0))
 
         self.folder_entry = ttk.Entry(self.content_frame, textvariable=self.folder_path, width=50)
-        self.folder_entry.grid(row=0, column=1)
+        self.folder_entry.grid(row=0, column=1, pady=(10, 0))
 
         self.browse_button = ttk.Button(self.content_frame, text="Browse", command=self.browse_folder)
-        self.browse_button.grid(row=0, column=2)
+        self.browse_button.grid(row=0, column=2, pady=(10, 0))
 
         self.canvas = Canvas(self.content_frame, width=512, height=512, bg="#464646", highlightbackground="#464646")
-        self.canvas.grid(row=1, column=0, columnspan=3)
+        self.canvas.grid(row=1, column=0, columnspan=4)
 
         self.apply_to_all_label = ttk.Label(self.content_frame, text="Apply to all:")
         self.apply_to_all_label.grid(row=3, column=0, sticky=E)
@@ -88,43 +94,48 @@ class ImageCaptionGUI(ThemedTk):
         self.replace_button = ttk.Button(self.content_frame, text="Replace", command=self.replace_all)
         self.replace_button.grid(row=5, column=2)
 
-        self.caption_padding = ttk.Label(self.content_frame, text="", padding=(0, 10))
-        self.caption_padding.grid(row=6, column=0)
+        #self.caption_padding = ttk.Label(self.content_frame, text="", padding=(0, 10))
+        #self.caption_padding.grid(row=6, column=0)
 
         self.caption_label = ttk.Label(self.content_frame, text="Caption:")
-        self.caption_label.grid(row=7, column=0, sticky=E)
+        self.caption_label.grid(row=6, column=0, sticky=E, pady=15)
 
         self.caption_entry = Text(self.content_frame, height=6, width=55, bg="#414141", fg="#a6a6a6", highlightbackground="#2a2a2a", highlightcolor="#2a2a2a")
         self.caption_entry.configure(insertbackground="white", highlightthickness=1, relief=SOLID, padx=5, pady=5)
-        self.caption_entry.grid(row=7, column=1, sticky=W)
-
+        self.caption_entry.grid(row=6, column=1, sticky=W, pady=15)
 
         self.save_button = ttk.Button(self.content_frame, text="Save this", command=self.save_caption)
-        self.save_button.grid(row=7, column=2)
+        self.save_button.grid(row=6, column=2, pady=15)
 
-        self.caption_padding = ttk.Label(self.content_frame, text="", padding=(0, 10))
-        self.caption_padding.grid(row=8, column=0)
-
+        #self.caption_padding = ttk.Label(self.content_frame, text="", padding=(0, 10))
+        #self.caption_padding.grid(row=8, column=0)
 
         self.backup_button = ttk.Button(self.content_frame, text="Backup txt-files", command=self.backup_txt_files)
-        self.backup_button.grid(row=9, column=0, sticky=W)
+        self.backup_button.grid(row=8, column=0, sticky=W, pady=(15, 0))
 
         self.create_caption_button = ttk.Button(self.content_frame, text="Create txt-file for each img", command=self.create_caption_files)
-        self.create_caption_button.grid(row=9, column=1, sticky=W)
+        self.create_caption_button.grid(row=8, column=1, sticky=W, pady=(15, 0))
 
-        self.auto_save_check = ttk.Checkbutton(self.content_frame, text="Save when moving", variable=self.auto_save_mode)
-        self.auto_save_check.grid(row=9, column=1, sticky=E)
+        self.auto_save_check = ttk.Checkbutton(self.content_frame, text="save when moving via nav keys <->", variable=self.auto_save_mode)
+        self.auto_save_check.grid(row=8, column=1, sticky=E, pady=(15, 0), padx=10)
 
         self.prev_button = ttk.Button(self.content_frame, text="Previous", command=self.show_prev_image)
-        self.prev_button.grid(row=9, column=2)
+        self.prev_button.grid(row=8, column=2, pady=(15, 0))
 
         self.next_button = ttk.Button(self.content_frame, text="Next", command=self.show_next_image)
-        self.next_button.grid(row=9, column=3)
+        self.next_button.grid(row=8, column=3, pady=(15, 0))
+
+        self.slider = ttk.Scale(self.content_frame, from_=0, to=len(self.image_files)-1, command=self.update_slider, length=400, orient='horizontal')
+        self.slider.grid(row=9, column=0, columnspan=4, sticky='ew', padx=22, pady=22)
+
+
+    def update_slider(self, value):
+        self.image_index = int(round(float(value)))
+        self.show_image()
 
     def save_and_show_done(self):
         self.save_caption()
         self.show_done_message(self.save_button, "Save this")
-
 
     def show_done_message(self, button, original_text):
         button.config(text="Done!")
@@ -154,7 +165,9 @@ class ImageCaptionGUI(ThemedTk):
 
         self.folder_path.set(folder)
         self.image_files = sorted(glob.glob(os.path.join(folder, "*.*g")))
+        self.image_count_label.config(text=f"[image 1 of {len(self.image_files)}]")
         self.image_index = 0
+        self.slider.configure(to=len(self.image_files)-1) # update the maximum value of the slider
         self.show_image()
 
     def show_image(self):
@@ -166,8 +179,20 @@ class ImageCaptionGUI(ThemedTk):
         img.thumbnail((512, 512), Image.LANCZOS)
 
         self.img_tk = ImageTk.PhotoImage(img)
-        self.canvas.create_image(0, 0, anchor=NW, image=self.img_tk)
+        
+        # calculate the x and y coordinates for the image
+        x = (self.canvas.winfo_width() - self.img_tk.width()) // 2
+        y = (self.canvas.winfo_height() - self.img_tk.height()) // 2
+
+        # Delete the old image
+        if self.image_on_canvas is not None:
+            self.canvas.delete(self.image_on_canvas)
+        
+        # Create a new image and store its id
+        self.image_on_canvas = self.canvas.create_image(x, y, anchor=NW, image=self.img_tk)
+
         self.image_file_label.config(text=self.truncate_filename(os.path.basename(img_path)))
+        self.image_count_label.config(text=f"[image {self.image_index + 1} of {len(self.image_files)}]")
 
         txt_path = self.get_txt_path(img_path)
         if os.path.exists(txt_path):
@@ -176,8 +201,6 @@ class ImageCaptionGUI(ThemedTk):
                 self.caption_entry.insert("1.0", f.read()) # insert new caption text from file
         else:
             self.caption_entry.delete("1.0", "end") # clear caption text if file doesn't exist
-
-
 
     def remove_from_all(self):
         modify_str = self.find_entry.get()
@@ -205,8 +228,6 @@ class ImageCaptionGUI(ThemedTk):
         # add delay before updating the image caption
         self.after(500, self.show_image)
 
-
-
     def show_prev_image(self):
         if not self.image_files:
             return
@@ -218,6 +239,7 @@ class ImageCaptionGUI(ThemedTk):
         if self.image_index < 0:
             self.image_index = len(self.image_files) - 1
 
+        self.slider.set(self.image_index)  # update the value of the slider
         self.show_image()
 
     def show_next_image(self):
@@ -231,8 +253,8 @@ class ImageCaptionGUI(ThemedTk):
         if self.image_index >= len(self.image_files):
             self.image_index = 0
 
+        self.slider.set(self.image_index)  # update the value of the slider
         self.show_image()
-
 
     def create_caption_files(self):
         self.create_caption_button.config(text="Working...")
@@ -269,9 +291,6 @@ class ImageCaptionGUI(ThemedTk):
         # add delay before updating the image caption
         self.after(500, self.show_image)
 
-
-
-
     def get_txt_path(self, img_path):
         base, ext = os.path.splitext(img_path)
         return base + '.txt'
@@ -290,7 +309,6 @@ class ImageCaptionGUI(ThemedTk):
             f.write(self.caption_entry.get("1.0", "end-1c"))
 
         self.show_done_message(self.save_button, "Save this")
-
 
     def on_closing(self):
         self.save_caption()
@@ -337,7 +355,6 @@ class ImageCaptionGUI(ThemedTk):
 
         self.show_done_message(self.prepend_button, "Prepend")
 
-
     def append_to_all(self):
         modify_str = self.apply_to_all_entry.get()
 
@@ -378,7 +395,6 @@ class ImageCaptionGUI(ThemedTk):
                 shutil.copy(txt_path, backup_folder)
 
         self.show_done_message(self.backup_button, "Backup txt-files")
-
 
 if __name__ == "__main__": 
     gui = ImageCaptionGUI() 
